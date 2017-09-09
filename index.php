1e88de49e0fba7aa66a2d0a554108aae88c665e7
Mickael <brenoit.mickael@gmail.com>
2017-09-09 08:33:57 +0200

    Two login forms created
    One will be completely secured, the other one will not be protected against some sort of attacks like SQL injections

--- index.php
+++ index.php
@@ -32,6 +32,58 @@
 
     <body>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+
+                    <h2 style="text-align:center;">Sans Sécurité</h2>
+
+                    <form>
+                          <div class="form-group">
+                            <label for="labelLogin">Login</label>
+                            <input type="text" class="form-control" id="loginWithoutSecurity" placeholder="Login">
+                          </div>
+                          <div class="form-group" id="divWithoutSecurity">
+                            <label for="labelPassword">Password</label>
+                            <input type="password" class="form-control" id="passwordWithoutSecurity" placeholder="Password">
+                          </div>
+                          <button type="submit" id="submitWithoutSecurity" class="btn btn-danger">Submit Without Security</button>
+                    </form>
+
+                </div>
+                
+                <div class="col-md-6">
+                    
+                    <h2 style="text-align:center;">Avec Sécurité</h2>
+
+                    <form>
+                          <div class="form-group">
+                            <label for="labelLogin">Login</label>
+                            <input type="text" class="form-control" id="loginWithSecurity" placeholder="Login">
+                          </div>
+                          <div class="form-group has-error" id="divWithSecurity">
+                            <label for="labelPassword" >Password - 8 chars & 1 digit & 1 uppercase & 1 lowercase min</label>
+                            <input type="password" class="form-control" id="passwordWithSecurity" placeholder="Password">
+                          </div>
+                          <button type="submit" id="submitWithSecurity" class="btn btn-success disabled">Submit With Security</button>
+                    </form>
+
+                </div>
+
+                <div class="col-md-6">
+                    
+                    <div id="displayWithoutSecurity"></div>
+
+                </div>
+
+                <div class="col-md-6">
+                    
+                    <div id="displayWithSecurity"></div>
+
+                </div>
+            </div>
+        </div>
+
         <!-- JQuery JS -->
         <script src="js/jquery.js"></script>
 
