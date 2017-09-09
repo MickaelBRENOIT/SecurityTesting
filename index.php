1bc20831de8cafb2626f08a11623f96efb460866
Mickael <brenoit.mickael@gmail.com>
2017-09-09 08:24:58 +0200

    Basic HTML5 template

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+    
+        <!-- allow to use accents -->
+        <meta charset="utf-8">
+        
+        <!-- Basic SEO Optimization -->
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        
+        <!-- allow compatibility with IE -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <!-- allow compatibility with small devices like tablets and phones -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- title of the website -->
+        <title>Security tests</title>
+        
+        <!-- icon of the website -->
+        <link rel="shortcut icon" href="">
+        
+        <!-- Fonts : need to be load before any others css files (Default Font "Roboto") -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
